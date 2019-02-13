286504d0893f3573d76a26e37df719bca27fa8ca
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-12 22:14:46 -0800

    bringing context into ImageManager

--- Insta.NET/Insta.NET/Models/Services/ImageManager.cs
+++ Insta.NET/Insta.NET/Models/Services/ImageManager.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using InstaDOTNET.Data;
+using InstaDOTNET.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InstaDOTNET.Models.Services
 {
-    public class ImageManager
+    public class ImageManager : IImage
     {
+        private readonly InstaDOTNETDbContext _context;
+
+        public ImageManager(InstaDOTNETDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Image> FindImage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Image>> GetImages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Image image)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
