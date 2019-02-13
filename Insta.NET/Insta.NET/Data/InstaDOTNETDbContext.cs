41ecb86acc3ce1858ff6ca976d8fd3d9ff9e335b
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-12 22:06:06 -0800

    Image model and properties added

--- Insta.NET/Insta.NET/Data/InstaDOTNETDbContext.cs
+++ Insta.NET/Insta.NET/Data/InstaDOTNETDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InstaDOTNET.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace Insta.NET.Data
         {
 
         }
+
+        public DbSet<Image> Images { get; set; }
     }
 }
