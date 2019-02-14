6948797a002e2a9093bf70915d52cc4923f5d371
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-14 15:03:44 -0800

    commented out Comment work to move forward w/ Blob storage

--- Insta.NET/Insta.NET/Models/Interfaces/IComment.cs
+++ Insta.NET/Insta.NET/Models/Interfaces/IComment.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace InstaDOTNET.Models.Interfaces
-{
-    interface IComment
-    {
-        // Delete
-        Task DeleteAsync(int id);
+//namespace InstaDOTNET.Models.Interfaces
+//{
+//    interface IComment
+//    {
+//        // Create
+//        Task CreateComment(Comment comment);
 
-        // Find
-        Task<Comment> FindCommentAsync(int id);
+//        // Find
+//        Task<Comment> FindCommentAsync(int id);
 
-        // GetAll
-        Task<List<Comment>> GetCommentsAsync();
+//        // GetAll
+//        Task<List<Comment>> GetCommentsAsync();
 
-        // Save
-        Task SaveAsync(Comment comment);
-    }
-}
+//        // Save
+//        Task SaveAsync(Comment comment);
+
+//        // Delete
+//        Task DeleteAsync(int id);
+//    }
+//}
--- Insta.NET/Insta.NET/Models/Services/CommentManager.cs
+++ Insta.NET/Insta.NET/Models/Services/CommentManager.cs
@@ -1,41 +1,47 @@
-﻿using InstaDOTNET.Data;
-using InstaDOTNET.Models.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using InstaDOTNET.Data;
+//using InstaDOTNET.Models.Interfaces;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace InstaDOTNET.Models.Services
-{
-    public class CommentManager : IComment
-    {
-        private readonly InstaDOTNETDbContext _context;
+//namespace InstaDOTNET.Models.Services
+//{
+//    public class CommentManager : IComment
+//    {
+//        private readonly InstaDOTNETDbContext _context;
 
-        public CommentManager(InstaDOTNETDbContext context)
-        {
-            _context = context;
-        }
+//        public CommentManager(InstaDOTNETDbContext context)
+//        {
+//            _context = context;
+//        }
 
-        public async Task DeleteAsync(int id)
-        {
-            Comment comment = _context.Comments.FirstOrDefault(c => c.ID == id);
-            _context.Comments.Remove(comment);
-            await _context.SaveChangesAsync();
-        }
+//        public async Task CreateComment(Comment comment)
+//        {
+//            _context.Comments.Add(comment);
+//            await _context.SaveChangesAsync();
+//        }
 
-        public Task<Comment> FindCommentAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public async Task DeleteAsync(int id)
+//        {
+//            Comment comment = _context.Comments.FirstOrDefault(c => c.ID == id);
+//            _context.Comments.Remove(comment);
+//            await _context.SaveChangesAsync();
+//        }
 
-        public Task<List<Comment>> GetCommentsAsync()
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<Comment> FindCommentAsync(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task SaveAsync(Comment comment)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public Task<List<Comment>> GetCommentsAsync()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public Task SaveAsync(Comment comment)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
