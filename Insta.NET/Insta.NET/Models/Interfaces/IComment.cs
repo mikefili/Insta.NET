12d05cf48bed3cc231095d12c8619dc21106446f
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-14 14:07:57 -0800

    IComment interface created

---
+++ Insta.NET/Insta.NET/Models/Interfaces/IComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaDOTNET.Models.Interfaces
+{
+    interface IComment
+    {
+        // Delete
+        Task DeleteAsync(int id);
+
+        // Find
+        Task<Comment> FindCommentAsync(int id);
+
+        // GetAll
+        Task<List<Comment>> GetCommentsAsync();
+
+        // Save
+        Task SaveAsync(Comment comment);
+    }
+}
---
+++ Insta.NET/Insta.NET/Models/Services/CommentManager.cs
@@ -0,0 +1,31 @@
+﻿using InstaDOTNET.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaDOTNET.Models.Services
+{
+    public class CommentManager : IComment
+    {
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment> FindCommentAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Comment>> GetCommentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
