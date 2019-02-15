46f51871e9a44b3d87e432d2de2fb607d25573d5
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-14 21:43:41 -0800

    added Comments pages & initial setup of .cshtml.cs file

---
+++ Insta.NET/Insta.NET/Pages/Images/Comments.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstaDOTNET.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InstaDOTNET.Pages.Images
+{
+    public class CommentsModel : PageModel
+    {
+        private readonly IComment _comment;
+
+        public CommentsModel(IComment comment)
+        {
+            _comment = comment;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
