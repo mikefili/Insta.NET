0de3b8cee48f6770de4fedf265e0f2a214bd0c6a
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-18 11:07:57 -0800

    prepped for initial deploy

---
+++ Insta.NET/Insta.NET/Migrations/20190215192026_update.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InstaDOTNET.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "URL",
+                table: "Images",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "URL",
+                table: "Images",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
