98e5d19a9648ae543567eafaedb2d0052cffb18d
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-18 21:14:04 -0800

    CanGetAllImages test passes

--- Insta.NET/UnitTests_Insta.NET/UnitTests_Services.cs
+++ Insta.NET/UnitTests_Insta.NET/UnitTests_Services.cs
@@ -11,8 +11,43 @@ namespace UnitTests_InstaDOTNET
 {
     public class UnitTests_Services
     {
+        // IMAGES
         [Fact]
-        public async void CanPostImage()
+        public async void CanGetAllImages()
+        {
+            DbContextOptions<InstaDOTNETDbContext> options = new DbContextOptionsBuilder<InstaDOTNETDbContext>().UseInMemoryDatabase("GetImagesAsync").Options;
+
+            using (InstaDOTNETDbContext context = new InstaDOTNETDbContext(options))
+            {
+                Image image = new Image();
+                image.ID = 1;
+                image.Name = "Test Image";
+                image.Author = "Test Author";
+                image.Caption = "Test Caption";
+                image.URL = "www.testURL.com/test-image.png";
+
+                Image image2 = new Image();
+                image2.ID = 2;
+                image2.Name = "Test Image 2";
+                image2.Author = "Test Author 2";
+                image2.Caption = "Test Caption 2";
+                image2.URL = "www.testURL.com/test-image-2.png";
+
+                ImageManager imageManager = new ImageManager(context);
+                await imageManager.SaveAsync(image);
+                await imageManager.SaveAsync(image2);
+                List<Image> images = new List<Image>();
+                images.Add(image);
+                images.Add(image2);
+
+                List<Image> result = await imageManager.GetImagesAsync();
+
+                Assert.Equal(images, result);
+            }
+        }
+
+        [Fact]
+        public async void CanSaveImage()
         {
             DbContextOptions<InstaDOTNETDbContext> options = new DbContextOptionsBuilder<InstaDOTNETDbContext>().UseInMemoryDatabase("SaveAsync").Options;
 
@@ -27,7 +62,9 @@ namespace UnitTests_InstaDOTNET
 
                 ImageManager imageManager = new ImageManager(context);
                 await imageManager.SaveAsync(image);
-                var result = await context.Images.FirstOrDefaultAsync(i => i.ID == image.ID);
+
+                Image result = await context.Images.FirstOrDefaultAsync(i => i.ID == image.ID);
+
                 Assert.Equal(image, result);
             }
         }
